ae84860c8946bd7266485497fa997085dd9cc858
divyanshu-012 <kapariyadiv@gmailcom>
2024-03-16 02:40:25 +0530

    testing map

--- Testing/map.js
+++ Testing/map.js
@@ -1,8 +1,46 @@
-const input = [1, 2, 3, 4, 5];
+const people = [
+    {
+        name: "john",
+        age: 20,
+        height: 169
 
-function transform(i) {
-    return i * 2;
-}
+    },
+    {
+        name: "Whie",
+        age: 22,
+        height: 163
 
-const ans = input.map(transform);
-console.log(ans);
+    },
+    {
+        name: "walker",
+        age: 28,
+        height: 189
+
+    },
+
+]
+
+const getAges = (person) => person.age * 2;
+
+
+
+// this is main syntax of map -->
+const ages = people.map(getAges);
+console.log(ages);
+
+
+const newPeople = people.map((item) => {
+    return {
+        firstName: item.name.toUpperCase(),
+        oldAge: item.age + 20,
+
+    }
+
+
+});
+console.log(newPeople)
+
+// in mapping
+// returns a new array
+// the size of array is same as original array
+// uses values of original array when making new one 
